e67796ff18f04b488b71b9f01783771c0bc1b97b
hick <hick1213@126.com>
2018-12-08 18:00:08 +0800

    Update OLLVM_Build.sh

--- OLLVM_Build.sh
+++ OLLVM_Build.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
+
+git clone -b llvm4.0 https://github.com/obfuscator-llvm/obfuscator
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ../obfuscator/
+make -j8
+
 NDK=$ANDROID_NDK
 if [ ! -n "$NDK" ];then
-	echo 'ndk is not set'
-	exit -1
+        echo 'ndk is not set'
+        exit -1
 fi
 toolcharin=$NDK/toolchains/ollvm-4.0/prebuilt/drawin-x86_64
 mkdir -p $toolcharin
-mv * $toolcharin
+mv -rf bin $toolcharin
+mv -rf lib $toolcharin
 
 mkdir -p $NDK/build/core/toolchains/arm-linux-androideabi-clang-ollvm4.0
 cp $NDK/build/core/toolchains/arm-linux-androideabi-clang/* $NDK/build/core/toolchains/arm-linux-androideabi-clang-ollvm4.0
