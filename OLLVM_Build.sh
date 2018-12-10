e84c6d60507b6e4234b4f5675353e3725f7d33da
hick <hick1213@126.com>
2018-12-10 19:02:21 +0800

    Update OLLVM_Build.sh

--- OLLVM_Build.sh
+++ OLLVM_Build.sh
@@ -1,44 +1,64 @@
 #!/bin/sh
-
-git clone -b llvm4.0 https://github.com/obfuscator-llvm/obfuscator
-mkdir build
+git clone -b llvm-4.0 https://github.com/obfuscator-llvm/obfuscator
+mkdir build 
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ../obfuscator/
 make -j8
 
 NDK=$ANDROID_NDK
 if [ ! -n "$NDK" ];then
-        echo 'ndk is not set'
-        exit -1
+	echo 'ndk is not set'
+	exit -1
 fi
-toolcharin=$NDK/toolchains/ollvm-4.0/prebuilt/drawin-x86_64
+toolcharin=$NDK/toolchains/ollvm-4.0/prebuilt/darwin-x86_64
 mkdir -p $toolcharin
-mv -rf bin $toolcharin
-mv -rf lib $toolcharin
+mv bin $toolcharin
+mv lib $toolcharin
 
-mkdir -p $NDK/build/core/toolchains/arm-linux-androideabi-clang-ollvm4.0
-cp $NDK/build/core/toolchains/arm-linux-androideabi-clang/* $NDK/build/core/toolchains/arm-linux-androideabi-clang-ollvm4.0
-mkdir -p $NDK/build/core/toolchains/aarch64-linux-android-clang-ollvm4.0
-cp $NDK/build/core/toolchains/aarch64-linux-android-clang/* $NDK/build/core/toolchains/aarch64-linux-android-clang-ollvm4.0
-mkdir -p $NDK/build/core/toolchains/x86-clang-ollvm4.0
-cp $NDK/build/core/toolchains/x86-clang/* $NDK/build/core/toolchains/x86-clang-ollvm4.0
-mkdir -p $NDK/build/core/toolchains/x86_64-clang-ollvm4.0
-cp $NDK/build/core/toolchains/x86_64-clang/* $NDK/build/core/toolchains/x86_64-clang-ollvm4.0
+sed -i "" 's/\$(call __ndk_error,Expected two items in TARGET_TOOLCHAIN_LIST/#&/' $NDK/build/core/setup-toolchain.mk
+sed -i "" 's/\found "\$(TARGET_TOOLCHAIN_LIST/#&/' $NDK/build/core/setup-toolchain.mk
+
+mkdir -p $NDK/build/core/toolchains/arm-linux-androideabi-clang_ollvm4.0
+cp $NDK/build/core/toolchains/arm-linux-androideabi-clang/* $NDK/build/core/toolchains/arm-linux-androideabi-clang_ollvm4.0
+mkdir -p $NDK/build/core/toolchains/aarch64-linux-android-clang_ollvm4.0
+cp $NDK/build/core/toolchains/aarch64-linux-android-clang/* $NDK/build/core/toolchains/aarch64-linux-android-clang_ollvm4.0
+mkdir -p $NDK/build/core/toolchains/x86-clang_ollvm4.0
+cp $NDK/build/core/toolchains/x86-clang/* $NDK/build/core/toolchains/x86-clang_ollvm4.0
+mkdir -p $NDK/build/core/toolchains/x86_64-clang_ollvm4.0
+cp $NDK/build/core/toolchains/x86_64-clang/* $NDK/build/core/toolchains/x86_64-clang_ollvm4.0
 
 
-sed -i "" 's/^TOOLCHAIN_NAME.*/TOOLCHAIN_NAME := ollvm-4.0/' $NDK/build/core/toolchains/arm-linux-androideabi-clang-ollvm4.0/setup.mk
-sed -i "" 's/^TOOLCHAIN_ROOT.*/TOOLCHAIN_ROOT := $(call get-toolchain-root,$(TOOLCHAIN_NAME))/' $NDK/build/core/toolchains/arm-linux-androideabi-clang-ollvm4.0/setup.mk
-sed -i "" 's/^TOOLCHAIN_PREFIX.*/TOOLCHAIN_PREFIX := $(TOOLCHAIN_ROOT)\/bin/' $NDK/build/core/toolchains/arm-linux-androideabi-clang-ollvm4.0/setup.mk
+function addContent(){
+	sed -i "" "/^TOOLCHAIN_PREFIX/a\\
+	TARGET_CXX := \$(LLVM_TOOLCHAIN_PREFIX)clang++\\
+	" $1
 
-sed -i "" 's/^TOOLCHAIN_NAME.*/TOOLCHAIN_NAME := ollvm-4.0/' $NDK/build/core/toolchains/aarch64-linux-android-clang-ollvm4.0/setup.mk
-sed -i "" 's/^TOOLCHAIN_ROOT.*/TOOLCHAIN_ROOT := $(call get-toolchain-root,$(TOOLCHAIN_NAME))/' $NDK/build/core/toolchains/aarch64-linux-android-clang-ollvm4.0/setup.mk
-sed -i "" 's/^TOOLCHAIN_PREFIX.*/TOOLCHAIN_PREFIX := $(TOOLCHAIN_ROOT)\/bin/' $NDK/build/core/toolchains/aarch64-linux-android-clang-ollvm4.0/setup.mk
+	sed -i "" "/^TOOLCHAIN_PREFIX/a\\
+	TARGET_CC := \$(LLVM_TOOLCHAIN_PREFIX)clang\\
+	" $1
 
-sed -i "" 's/^TOOLCHAIN_NAME.*/TOOLCHAIN_NAME := ollvm-4.0/' $NDK/build/core/toolchains/x86-clang-ollvm4.0/setup.mk
-sed -i "" 's/^TOOLCHAIN_ROOT.*/TOOLCHAIN_ROOT := $(call get-toolchain-root,$(TOOLCHAIN_NAME))/' $NDK/build/core/toolchains/x86-clang-ollvm4.0/setup.mk
-sed -i "" 's/^TOOLCHAIN_PREFIX.*/TOOLCHAIN_PREFIX := $(TOOLCHAIN_ROOT)\/bin/' $NDK/build/core/toolchains/x86-clang-ollvm4.0/setup.mk
+	sed -i "" "/^TOOLCHAIN_PREFIX/a\\
+	LLVM_TOOLCHAIN_PREFIX := \$(LLVM_TOOLCHAIN_PREBUILT_ROOT)/bin/\\
+	" $1
 
-sed -i "" 's/^TOOLCHAIN_NAME.*/TOOLCHAIN_NAME := ollvm-4.0/' $NDK/build/core/toolchains/x86_64-clang-ollvm4.0/setup.mk
-sed -i "" 's/^TOOLCHAIN_ROOT.*/TOOLCHAIN_ROOT := $(call get-toolchain-root,$(TOOLCHAIN_NAME))/' $NDK/build/core/toolchains/x86_64-clang-ollvm4.0/setup.mk
-sed -i "" 's/^TOOLCHAIN_PREFIX.*/TOOLCHAIN_PREFIX := $(TOOLCHAIN_ROOT)\/bin/' $NDK/build/core/toolchains/x86_64-clang-ollvm4.0/setup.mk
+	sed -i "" "/^TOOLCHAIN_PREFIX/a\\
+	LLVM_TOOLCHAIN_PREBUILT_ROOT := \$(call get-toolchain-root,\$(LLVM_TOOLCHAIN_ROOT))\\
+	" $1
 
+	sed -i "" "/^TOOLCHAIN_PREFIX/a\\
+	LLVM_TOOLCHAIN_ROOT := \$(LLVM_NAME)\\
+	" $1
+
+	sed -i "" "/^TOOLCHAIN_PREFIX/a\\
+	LLVM_NAME := ollvm-\$(LLVM_VERSION)\\
+	" $1
+
+	sed -i "" "/^TOOLCHAIN_PREFIX/a\\
+	LLVM_VERSION := 4.0\\
+	" $1
+}
+
+addContent $NDK/build/core/toolchains/arm-linux-androideabi-clang_ollvm4.0/setup.mk
+addContent $NDK/build/core/toolchains/aarch64-linux-android-clang_ollvm4.0/setup.mk
+addContent $NDK/build/core/toolchains/x86-clang_ollvm4.0/setup.mk
+addContent $NDK/build/core/toolchains/x86_64-clang_ollvm4.0/setup.mk
